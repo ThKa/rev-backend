cc92490766a4dff59003362bdfbfada24cc2fbf3
ThKa <thomas.kamps@youpers.com>
2015-01-16 10:36:06 +0100

    Update request_model.js
    With attributes and categories exported to the "enum"-section.

--- src/models/request_model.js
+++ src/models/request_model.js
@@ -17,7 +17,12 @@ var RequestSchema = common.newSchema({
     status: { type: String, enum: enums.requestStatus, default: "open" },
     location: {type: [Number], index: '2d'},
     further-description: {type: String, trim: true, i18n: true, required: false},
-    category: { type: String, enum: ['SmRe', 'Comp', 'Gard', 'Baby', 'AniC', 'OfWo', 'TecS']}
+    category: { type: String, enum: enums.requestCategories, default: "Gard"},
+    creationDate: { type: Date, default: Date.now }, // today
+    expieryDate: { type: Date, default: Date.now + 2d }, // today plus 2 days
+    earliestWorkStartDate: { type: Date, default: Date.now }, 
+    latestWorkStartDate: { type: Date, default: Date.now + 2d }, // today plus 2 days
+    workDuration:  { type: Number, min: 1, default: 60 } // Estimated working time in minutes
 });
 
 module.exports = mongoose.model('Request', RequestSchema);
