8c68aa9dc9d239914fad35f326a313016fd044c5
ThKa <thomas.kamps@youpers.com>
2015-01-16 11:19:44 +0100

    Update enums.js

--- src/models/enums.js
+++ src/models/enums.js
@@ -1,5 +1,8 @@
 module.exports = {
 
-        requestStatus: "open".split(' ')
+        requestStatus: "open requested providerAccepted cancelled expired".split(' '),
+        applicationStatus: "applied denied accepted cancelled expired".split(' '),
+        feedbackRating: "high med low".split(' '),
+        requestCategorie: "SmRe Tran Comp Gard Baby BurH AniC OfWo TecS Shop HoWo".split(' ')
 
 };
