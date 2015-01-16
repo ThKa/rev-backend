6c006a7621f24b18a1b2185daf67cc8b9fa927a9
ThKa <thomas.kamps@youpers.com>
2015-01-16 10:49:16 +0100

    Create application_model.js
    Application to a request.

---
+++ src/models/application_model.js
@@ -0,0 +1,23 @@
+/**
+ * Module dependencies.
+ */
+var mongoose = require('ypbackendlib').mongoose,
+    common = require('ypbackendlib').commmonModels,
+    enums = require('./enums'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+/**
+ * Weigh In Schema
+ * @type {Schema}
+ */
+
+var ApplicationSchema = common.newSchema({
+    applicant: { type: ObjectId, ref: 'User', required: true },
+    request: { type: ObjectID, ref: 'Request', required: true },
+    status: { type: String, enum: enums.applicationStatus, default: "applied" },
+    applicationTimestamp: { type: Date, default: Date.now }, // now
+    
+});
+
+module.exports = mongoose.model('Application', ApplicationSchema);
